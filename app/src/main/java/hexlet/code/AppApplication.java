0281f2a9f6aae7d39a2e58c7b5f5bc34d67a1009
Dmitrii Polkovnikov <dpolkovnikov@microsoft.com>
2023-08-22 16:32:39 +0200

    Fix bug

--- app/src/main/java/hexlet/code/AppApplication.java
+++ app/src/main/java/hexlet/code/AppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class AppApplication {
+public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
